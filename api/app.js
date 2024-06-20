0c352355c0c0d9ab5ff8d8ed439ce02b9f2381c3
Juan <workpulgarin@gmail.com>
2024-06-19 19:42:25 -0500

    r

--- api/app.js
+++ api/app.js
@@ -1,9 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import fs from 'fs';
-import https from 'https';
-import http from 'https';
+import http from 'http';
 import { Server } from 'socket.io';
 import cors from "cors";
 import { connectDb } from "./db.js";
@@ -12,25 +10,18 @@ import hotel from "./src/routes/hotel.routes.js";
 import usuarios from "./src/routes/usuarios.routes.js";
 import dimaster from "./src/routes/dimaster.routes.js";
 import users from "./src/models/usuarios.models.js";
-import bcrypt from "bcryptjs";
-import { connectAndFetchData } from './src/controller/conectarPeriodica.js'; // Importa la función para tareas periódicas
-import dotenv from "dotenv";
-
-dotenv.config();
+import bcrypt from "bcryptjs"
+import { connectAndFetchData  } from './src/controller/conectarPeriodica.js'; // Importa la función para tareas periódicas
 
 const app = express();
 
-// Lee los certificados SSL desde variables de entorno
-const privateKey = Buffer.from(process.env.SSL_KEY_BASE64, 'base64').toString('utf-8');
-const certificate = Buffer.from(process.env.SSL_CERT_BASE64, 'base64').toString('utf-8');
-
-const credentials = {
-  key: privateKey,
-  cert: certificate
-};
-
-// Crea el servidor HTTPS
 const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"]
+//   }
+// });
 const io = new Server(server, {
   cors: {
     origin: "https://clientservidor.onrender.com",
@@ -43,19 +34,42 @@ app.use(morgan("dev"));
 // express.json me deja leer objetos json en las rutas
 app.use(express.json());
 app.use(cookieParser());
-
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
 app.use(cors({
   origin: "https://clientservidor.onrender.com",
-  methods: ["GET", "POST"],
- // allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-
 
 app.use("/api/v1", dimaster);
 app.use("/api/v1", habitaciones);
 app.use("/api/v1", hotel);
 app.use("/api/v1", usuarios);
+
+// const createAdminUser = async () => {
+//   try {
+//     const adminEmail = "diseven@diseven.com";
+//     const adminExists = await users.findOne({ email: adminEmail });
+//     const passwordHash = await bcrypt.hash("admin", 10)
+//     if (!adminExists) {
+//       const adminUser = new users({
+//         username: "admin",
+//         email: adminEmail,
+//         password: passwordHash, // Asegúrate de cifrar la contraseña antes de guardar
+//         rol: "Admin"
+//       });
+//       await adminUser.save();
+//       console.log("Usuario admin creado exitosamente.");
+//     } else {
+//       console.log("Usuario admin ya existe.");
+//     }
+//   } catch (error) {
+//     console.error("Error creando usuario admin:", error);
+//   }
+// };
+
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
@@ -63,7 +77,8 @@ server.listen(PORT, () => {
 
   // se conecta automaticamente a el topico de los hoteles
   connectAndFetchData();
-  // createAdminUser()
+//createAdminUser()
+
 });
 
 // import fernet from 'fernet';
@@ -82,6 +97,7 @@ server.listen(PORT, () => {
 // } catch (error) {
 //   console.error("Error parsing JSON:", error);
 // }
+
 
 connectDb();
 export { app, io };
