462e95b82fc0598cb65ab50edab182abeb0daed0
Juan <workpulgarin@gmail.com>
2024-06-22 09:01:16 -0500

    r

--- api/src/models/usuarios.models.js
+++ api/src/models/usuarios.models.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     },
     rol : {
         type : String,
-       default: "usuario"
+       default: "Admin"
     },
 
     hotel : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Hotel' }]
